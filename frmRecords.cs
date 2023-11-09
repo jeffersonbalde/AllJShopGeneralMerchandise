92250a4f5e3e94cfaea367e3949ba0cf92cbb8d7
jeffersonbalde <jeffersonbalde8@gmail.com>
2023-11-09 23:30:02 +0800

    design

--- frmRecords.cs
+++ frmRecords.cs
@@ -16,5 +16,10 @@ namespace OOP_System
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
     }
 }
