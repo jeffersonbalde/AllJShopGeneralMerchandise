48f1470f5967aa938b5eb784a28632574069f883
jeffersonbalde <jeffersonbalde8@gmail.com>
2023-12-03 00:30:04 +0800

    jeff test

--- TEST.Designer.cs
+++ TEST.Designer.cs
@@ -36,6 +36,7 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -109,11 +110,21 @@
             this.label6.TabIndex = 7;
             this.label6.Text = "textbox rechecking.... ";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(813, 78);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(180, 528);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "save";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // TEST
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(734, 549);
+            this.ClientSize = new System.Drawing.Size(1271, 677);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -139,5 +150,6 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
     }
 }
