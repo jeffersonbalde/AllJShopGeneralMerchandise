a2462bb6cf11d43f110bda46b4b65f44d6c35469
mjcagzzzz <mjcagzzzz@gmail.com>
2023-12-02 23:41:30 +0800

    MJ - added text in textbox and added label

--- TEST.Designer.cs
+++ TEST.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
@@ -37,12 +38,24 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(462, 22);
             this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Added text here";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(259, 148);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "And label here";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // TEST
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(636, 262);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Name = "TEST";
             this.Text = "TEST";
@@ -54,5 +67,6 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
