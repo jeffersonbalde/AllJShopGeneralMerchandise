5ccec019b2c2dd8aa6538b6ad4f2b7143ad95c11
Johannah123456 <147459032+Johannah123456@users.noreply.github.com>
2023-12-03 23:33:06 +0800

    panagdait

--- TEST.Designer.cs
+++ TEST.Designer.cs
@@ -38,39 +38,46 @@
             this.label6 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(73, 78);
+            this.textBox1.Location = new System.Drawing.Point(55, 63);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(462, 22);
+            this.textBox1.Size = new System.Drawing.Size(348, 20);
             this.textBox1.TabIndex = 0;
             this.textBox1.Text = "Changing the text in textbox";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(135, 37);
+            this.label1.Location = new System.Drawing.Point(101, 30);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(243, 16);
+            this.label1.Size = new System.Drawing.Size(193, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "bisan usa ang e add. Test form. -mayee";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(70, 394);
+            this.label2.Location = new System.Drawing.Point(52, 320);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(133, 16);
+            this.label2.Size = new System.Drawing.Size(112, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "textbox rechecking.... ";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(510, 415);
+            this.button1.Location = new System.Drawing.Point(382, 337);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(180, 65);
+            this.button1.Size = new System.Drawing.Size(135, 53);
             this.button1.TabIndex = 3;
             this.button1.Text = "save";
             this.button1.UseVisualStyleBackColor = true;
@@ -78,62 +85,101 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(507, 143);
+            this.label3.Location = new System.Drawing.Point(380, 116);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(133, 16);
+            this.label3.Size = new System.Drawing.Size(112, 13);
             this.label3.TabIndex = 4;
             this.label3.Text = "textbox rechecking.... ";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(507, 178);
+            this.label4.Location = new System.Drawing.Point(380, 145);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(133, 16);
+            this.label4.Size = new System.Drawing.Size(112, 13);
             this.label4.TabIndex = 5;
             this.label4.Text = "textbox rechecking.... ";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(507, 220);
+            this.label5.Location = new System.Drawing.Point(380, 179);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(133, 16);
+            this.label5.Size = new System.Drawing.Size(112, 13);
             this.label5.TabIndex = 6;
             this.label5.Text = "textbox rechecking.... ";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(314, 429);
+            this.label6.Location = new System.Drawing.Point(236, 349);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(133, 16);
+            this.label6.Size = new System.Drawing.Size(112, 13);
             this.label6.TabIndex = 7;
             this.label6.Text = "textbox rechecking.... ";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(760, 55);
+            this.button2.Location = new System.Drawing.Point(587, 48);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(180, 245);
+            this.button2.Size = new System.Drawing.Size(135, 199);
             this.button2.TabIndex = 8;
             this.button2.Text = "save";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(1001, 55);
+            this.button3.Location = new System.Drawing.Point(751, 45);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(180, 245);
+            this.button3.Size = new System.Drawing.Size(135, 199);
             this.button3.TabIndex = 9;
             this.button3.Text = "save";
             this.button3.UseVisualStyleBackColor = true;
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(42, 116);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(96, 138);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "save";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(159, 116);
+            this.button5.Margin = new System.Windows.Forms.Padding(2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(90, 138);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "save";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(267, 116);
+            this.button6.Margin = new System.Windows.Forms.Padding(2);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(90, 138);
+            this.button6.TabIndex = 12;
+            this.button6.Text = "save";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
             // TEST
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1271, 677);
+            this.ClientSize = new System.Drawing.Size(953, 550);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label6);
@@ -144,6 +190,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "TEST";
             this.Text = "TEST";
             this.ResumeLayout(false);
@@ -163,5 +210,8 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
