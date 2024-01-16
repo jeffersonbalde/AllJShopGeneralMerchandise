5e6aac1634d27f436e14c54224cf168e411dd3e6
jeffersonbalde <jeffersonbalde8@gmail.com>
2024-01-16 23:19:50 +0800

    added setuo exe

---
+++ JJAM.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace OOP_System
+{
+    partial class JJAM
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // JJAM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(998, 529);
+            this.Name = "JJAM";
+            this.Text = "JJAM";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
